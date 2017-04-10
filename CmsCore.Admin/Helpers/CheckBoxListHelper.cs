a5bd42b39c119c28a528921a85fddfce87a677f4
Nex0r <ertyeni@gmail.com>
2017-04-10 15:37:44 +0300

    Added Post Category Checkboxlist Jstree

--- CmsCore.Admin/Helpers/CheckBoxListHelper.cs
+++ CmsCore.Admin/Helpers/CheckBoxListHelper.cs
@@ -8,39 +8,39 @@ using System.Threading.Tasks;
 
 namespace CmsCore.Admin.Helpers
 {
-    [HtmlTargetElement("checkBoxList", TagStructure = TagStructure.NormalOrSelfClosing)]
-    public class CheckBoxListHelper:TagHelper
-    {
-        public string Name { get; set; }
-        public string Class { get; set; }
+    //[HtmlTargetElement("checkBoxList", TagStructure = TagStructure.NormalOrSelfClosing)]
+    //public class CheckBoxListHelper:TagHelper
+    //{
+    //    public string Name { get; set; }
+    //    public string Class { get; set; }
       
-        public MultiSelectList Items { get; set; }
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            output.TagMode = TagMode.StartTagAndEndTag;
-            output.TagName = "div";
+    //    public MultiSelectList Items { get; set; }
+    //    public override void Process(TagHelperContext context, TagHelperOutput output)
+    //    {
+    //        output.TagMode = TagMode.StartTagAndEndTag;
+    //        output.TagName = "div";
             
-            string group = "";
-            foreach (var item in Items)
-            {
-                TagBuilder checkbox = new TagBuilder("input");
-                checkbox.Attributes.Add("type", "checkbox");
-                checkbox.Attributes.Add("name", Name);
-                checkbox.Attributes.Add("class", Class);
-                checkbox.Attributes.Add("value", item.Value);
+    //        string group = "";
+    //        foreach (var item in Items)
+    //        {
+    //            TagBuilder checkbox = new TagBuilder("input");
+    //            checkbox.Attributes.Add("type", "checkbox");
+    //            checkbox.Attributes.Add("name", Name);
+    //            checkbox.Attributes.Add("class", Class);
+    //            checkbox.Attributes.Add("value", item.Value);
                 
-                if (item.Disabled)                
-                    checkbox.Attributes.Add("disabled", "disabled");
+    //            if (item.Disabled)                
+    //                checkbox.Attributes.Add("disabled", "disabled");
                 
-                if (item.Selected)
-                    checkbox.Attributes.Add("checked", "checked");
-                checkbox.InnerHtml.AppendHtml(item.Text);
-                var writer = new System.IO.StringWriter();
-                checkbox.WriteTo(writer, HtmlEncoder.Default);
-                output.PostContent.AppendHtml((item.Group!=null?"&nbsp;&nbsp;&nbsp;":"").ToString()+writer.ToString()+"<br />");
+    //            if (item.Selected)
+    //                checkbox.Attributes.Add("checked", "checked");
+    //            checkbox.InnerHtml.AppendHtml(item.Text);
+    //            var writer = new System.IO.StringWriter();
+    //            checkbox.WriteTo(writer, HtmlEncoder.Default);
+    //            output.PostContent.AppendHtml((item.Group!=null?"&nbsp;&nbsp;&nbsp;":"").ToString()+writer.ToString()+"<br />");
                 
-            }                           
+    //        }                           
                 
-            }
-        }
+    //        }
+    //    }
 }
