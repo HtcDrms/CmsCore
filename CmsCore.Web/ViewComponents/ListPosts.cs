c219c070e7f33af8f615698cb2974d97488f88ad
Murat Demirci <mdemirci@outlook.com>
2017-04-17 15:02:06 +0300

    listposts viewcomponent added

---
+++ CmsCore.Web/ViewComponents/ListPosts.cs
@@ -0,0 +1,30 @@
+﻿using CmsCore.Model.Entities;
+using CmsCore.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCore.Web.ViewComponents
+{
+    public class ListPosts:ViewComponent
+    {
+        private readonly IPostService postService;
+        public ListPosts(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string categoryNames = "", int count = 8)
+        {
+            var items = await GetItems(categoryNames, count);
+            return View(items);
+        }
+        private Task<List<Post>> GetItems(string categoryNames, int count)
+        {
+            List<Post> posts = postService.GetPostsByCategoryNames(categoryNames, count).ToList();
+            return Task.FromResult(posts);
+        }
+    }
+}
